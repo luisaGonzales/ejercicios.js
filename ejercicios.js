1f7c903aaa720ed6760c53d00f80b56b77e3086f
AQP20171679 <alejandraadriantejada@gmail.com>
2017-07-14 16:18:16 -0500

    Mi primer branche

--- ejercicios.js
+++ ejercicios.js
@@ -8,4 +8,17 @@
 
 //Ejercicio 5
 
-//Ejercicio 6
+//Ejercicio 6: Cuenta cuantas vocales tiene un array de palabras
+function cuentaVocales(array){
+  array=array.join("").split("");
+  var vocales="aeiouáéíóú";
+  var cuenta = 0;
+  for( var i in vocales){
+    for( var j in array){
+      if(vocales[i]==array[j].toLowerCase()){
+        cuenta++;
+      }
+    }
+  }
+  return cuenta;
+}
