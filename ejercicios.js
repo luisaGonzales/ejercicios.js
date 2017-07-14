94654eef8784ca18fe811ed8c67d26ebbcf9b461
aydee <aydee.qm@gmail.com>
2017-07-14 16:23:07 -0500

    tercer ejercicio

--- ejercicios.js
+++ ejercicios.js
@@ -3,6 +3,21 @@
 //Ejercicio 2
 
 //Ejercicio 3
+function contarCaracter(palabra){
+  var arr = [];
+  var contarLetra='a';
+  var contador= 0;
+  for(var i = 0; i<palabra.length; i++){
+    for(var j=0; j<palabra[i].length; j++){
+      if(palabra[i][j]==contarLetra){
+      contador ++;
+    }
+    }
+  } 
+  console.log(contador);
+  console.log(arr);
+}
+console.log(contarCaracter(['casa', 'mamamia']));
 
 //Ejercicio 4
 
