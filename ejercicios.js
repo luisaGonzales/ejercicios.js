fade6718c481b48887bd73f0f734e7b888742d5b
luisaGonzales <luisagc711@gmail.com>
2017-07-14 17:22:08 -0500

    Enviando x3

--- ejercicios.js
+++ ejercicios.js
@@ -76,3 +76,22 @@ function cuentaVocales(array){
   return sumatoria;
   }
 sumatoriaImpares (n, m);
+
+//Ejercicio 7
+//Ejercicio 7: Sumatoria de los números impares comprendidos en un rango
+var n = 1;
+var m = 7;
+function sumatoriaImpares (n,m){
+  var arr = [];
+  var sumatoria = 0;
+  for (var i = n; i <= m; i++){
+      arr.push(i);
+    }
+  for (var j = 0; j < arr.length; j++){
+    if(arr[j]%2 !== 0){
+      sumatoria = sumatoria + arr[j];
+    }
+  }
+  return sumatoria;
+  }
+sumatoriaImpares (n, m);
