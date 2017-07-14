df153365a13862bf7d47026d395c6dfcf41fef86
bariflor <barihuamachuco@gmail.com>
2017-07-14 16:57:42 -0500

    ejer4

--- ejercicios.js
+++ ejercicios.js
@@ -5,7 +5,10 @@
 //Ejercicio 3
 
 //Ejercicio 4
-
+String.prototype.reverse = function() {
+  return this.split("").reverse().join("");
+};
+console.log("roma".reverse()); // amor
 //Ejercicio 5
 
 //Ejercicio 6
