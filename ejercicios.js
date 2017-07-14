19d6617c505cc7da32994229929b323b6916f79f
luisaGonzales <luisagc711@gmail.com>
2017-07-14 16:33:30 -0500

    Ejercicio 17

--- ejercicios.js
+++ ejercicios.js
@@ -9,3 +9,37 @@
 //Ejercicio 5
 
 //Ejercicio 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Ejercicio 7
+function sumatoriaImpares (n,m){
+  var arr = [];
+  var sumatoria = 0;
+  for (var i = n; i <= m; i++){
+      arr.push(i);
+    }
+  for (var j = 0; j < arr.length; j++){
+    if(arr[j]%2 !== 0){
+      sumatoria = sumatoria + arr[j];
+    }
+  }
+  return sumatoria;
+  }
+sumatoriaImpares (n, m);
